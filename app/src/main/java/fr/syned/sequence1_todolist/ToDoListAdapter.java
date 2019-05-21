d866dd64212caa05e874dc483403ef1d027df962
NakedSyned <synedday@gmail.com>
2019-05-21 17:26:03 +0200

    Added RecyclerView for ToDoLists. Improvements : - Clearing add todolist text view after clicking on Add New ToDoList - Notifying dataset has changed when adding new ToDoList, in order to refresh display.

--- app/src/main/java/fr/syned/sequence1_todolist/ToDoListAdapter.java
+++ app/src/main/java/fr/syned/sequence1_todolist/ToDoListAdapter.java
@@ -1,9 +1,11 @@
 package fr.syned.sequence1_todolist;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -11,42 +13,38 @@ import java.util.List;
 class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.ToDoListViewHolder> {
     private List<ToDoList> mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ToDoListViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+
         public TextView textView;
-        public ToDoListViewHolder(TextView v) {
+
+        public ToDoListViewHolder(@NonNull View v) {
             super(v);
-            textView = v;
+            textView = v.findViewById(R.id.todolist_name);
+        }
+
+        public void bind(String item) {
+            textView.setText(item);
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public ToDoListAdapter(List<ToDoList> myDataset) {
-        mDataset = myDataset;
+    public ToDoListAdapter(List<ToDoList> dataset) {
+        mDataset = dataset;
     }
 
-    // Create new views (invoked by the layout manager)
+    @NonNull
     @Override
-    public ToDoListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.todolist_item, parent, false);
-        ToDoListViewHolder vh = new ToDoListViewHolder(v);
-        return vh;
+    public ToDoListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.todolist_item, parent, false);
+        return new ToDoListViewHolder(v);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ToDoListViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.textView.setText((mDataset.get(position)).getName());
+    public void onBindViewHolder(@NonNull ToDoListViewHolder holder, int position) {
+        holder.bind(mDataset.get(position).getName());
 
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mDataset.size();
