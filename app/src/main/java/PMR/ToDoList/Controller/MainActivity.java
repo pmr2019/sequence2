94a27b8746b5316e25dac3861061852e77110504
romain <romain.mabboux@gmail.com>
2019-05-26 18:23:23 +0100

    cc

--- app/src/main/java/PMR/ToDoList/Controller/MainActivity.java
+++ app/src/main/java/PMR/ToDoList/Controller/MainActivity.java
@@ -3,14 +3,40 @@ package PMR.ToDoList.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import PMR.ToDoList.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edtPseudo;
+    private Button btnPseudo;
+    private TextView txtPseudo;
+    private androidx.appcompat.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtPseudo = (EditText) findViewById(R.id.edtPseudo);
+        btnPseudo = (Button) findViewById(R.id.btnPseudo);
+        txtPseudo = (TextView) findViewById(R.id.txtPseudo);
+        toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menupseudo, menu);
+        return true;
+    }
+
+
 }
