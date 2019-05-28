99f95639d60cc1f9c6c7c67259ceedebacb898fd
Pierrick Gervasi <Pierrick Gervasi@gmail.com>
2019-05-28 16:07:24 +0200

    Insertion des boutons possible

--- app/src/main/java/PMR/ToDoList/Controller/ToDoListActivity.java
+++ app/src/main/java/PMR/ToDoList/Controller/ToDoListActivity.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,34 +17,75 @@ import PMR.ToDoList.R;
 
 public class ToDoListActivity extends AppCompatActivity {
 
+    // PARTIE TOOLBAR
+
     private androidx.appcompat.widget.Toolbar toolbar;
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    // PARTIE RECYCLERVIEW
 
-    private ArrayList<ToDoList> toDoListList;
+    private RecyclerView toDoListRecyclerView;
+    private RecyclerView.Adapter toDoListAdapter;
+    private RecyclerView.LayoutManager toDoListLayoutManager;
+
+    //PARTIE DONNEES
+
+    private ArrayList<ToDoList> toDoLists;
+
+    //INSERT TODOLIST
+
+    private Button btnInsertToDoList;
+    private EditText textInsertToDoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todolist);
 
-        toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        buildToolbar();
 
-        toDoListList=new ArrayList<>();
-        toDoListList.add(new ToDoList("Todo1Todo3Todo3Todo3Todo3Todo3"));
+        toDoLists =new ArrayList<>();
+
+        buildRecyclerView(toDoLists);
+
+        btnInsertToDoList=findViewById(R.id.btnInsertToDoList);
+        textInsertToDoList=findViewById(R.id.textInsertToDoList);
+
+        btnInsertToDoList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameToDoList= textInsertToDoList.getText().toString();
+
+                Toast toast=Toast.makeText(getApplicationContext(),nameToDoList,Toast.LENGTH_SHORT);
+                toast.show();
+
+                if (!nameToDoList.equals("")){
+                    insertTodoList(nameToDoList);
+                }
+            }
+        });
 
 
-        mRecyclerView = findViewById(R.id.rvTodoList);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ToDoListAdapter(toDoListList);
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
     }
+
+    public void buildRecyclerView(ArrayList<ToDoList> list){
+        toDoListRecyclerView = findViewById(R.id.rvTodoList);
+        toDoListRecyclerView.setHasFixedSize(true);
+        toDoListLayoutManager = new LinearLayoutManager(this);
+        toDoListAdapter = new ToDoListAdapter(list);
+        toDoListRecyclerView.setLayoutManager(toDoListLayoutManager);
+        toDoListRecyclerView.setAdapter(toDoListAdapter);
+    }
+
+    public void buildToolbar(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void insertTodoList(String nameToDoList){
+        toDoLists.add(new ToDoList(nameToDoList));
+        toDoListAdapter.notifyItemInserted(toDoListAdapter.getItemCount()-1);
+    }
+
 
 
 }
