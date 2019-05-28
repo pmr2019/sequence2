d73c6559d55ed91520df14a07691061b2592e67d
Pierrick Gervasi <Pierrick Gervasi@gmail.com>
2019-05-28 17:49:40 +0200

    Ajout du boutton Delete pour les to do list + intent pour aller sur la page des tâches

--- app/src/main/java/PMR/ToDoList/Controller/ToDoListActivity.java
+++ app/src/main/java/PMR/ToDoList/Controller/ToDoListActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,21 +19,17 @@ import PMR.ToDoList.R;
 public class ToDoListActivity extends AppCompatActivity {
 
     // PARTIE TOOLBAR
-
     private androidx.appcompat.widget.Toolbar toolbar;
 
     // PARTIE RECYCLERVIEW
-
     private RecyclerView toDoListRecyclerView;
-    private RecyclerView.Adapter toDoListAdapter;
+    private ToDoListAdapter toDoListAdapter;
     private RecyclerView.LayoutManager toDoListLayoutManager;
 
     //PARTIE DONNEES
-
     private ArrayList<ToDoList> toDoLists;
 
     //INSERT TODOLIST
-
     private Button btnInsertToDoList;
     private EditText textInsertToDoList;
 
@@ -50,13 +47,12 @@ public class ToDoListActivity extends AppCompatActivity {
         btnInsertToDoList=findViewById(R.id.btnInsertToDoList);
         textInsertToDoList=findViewById(R.id.textInsertToDoList);
 
+        //BOUTON D'INSERTION D'UNE TO DO LIST
         btnInsertToDoList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String nameToDoList= textInsertToDoList.getText().toString();
-
-                Toast toast=Toast.makeText(getApplicationContext(),nameToDoList,Toast.LENGTH_SHORT);
-                toast.show();
+                textInsertToDoList.setText("");
 
                 if (!nameToDoList.equals("")){
                     insertTodoList(nameToDoList);
@@ -74,6 +70,22 @@ public class ToDoListActivity extends AppCompatActivity {
         toDoListAdapter = new ToDoListAdapter(list);
         toDoListRecyclerView.setLayoutManager(toDoListLayoutManager);
         toDoListRecyclerView.setAdapter(toDoListAdapter);
+
+
+        toDoListAdapter.setOnItemClickListener(new ToDoListAdapter.OnItemClickListener() {
+            @Override
+            //BOUTON QUAND ON CLIQUE SUR UNE CARD
+            public void onItemClick(int position) {
+                //On récupère la TODOLIST en question
+                accessTasksActivity(position);
+            }
+            //BOUTON QUAND ON CLIQUE SUR DELETE
+            @Override
+            public void onDeleteClick(int position) {
+                toDoLists.remove(position);
+                toDoListAdapter.notifyItemRemoved(position);
+            }
+        });
     }
 
     public void buildToolbar(){
@@ -81,11 +93,23 @@ public class ToDoListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+
     public void insertTodoList(String nameToDoList){
         toDoLists.add(new ToDoList(nameToDoList));
         toDoListAdapter.notifyItemInserted(toDoListAdapter.getItemCount()-1);
     }
 
 
+
+    // FONCTION QUI FAIT DES TRUCS QUAND ON CLIQUE SUR UNE TO DO LIST
+    //ELLE PREND EN ARGUMENT LA POSITION DE LA TO DO LIST DANS LA LISTE DES TO DO LISTS
+
+    public void accessTasksActivity(int position){
+        //toDoLists.get(position).setTitreListeToDo("clicked");
+        //toDoListAdapter.notifyItemChanged(position);
+
+        Intent intent=new Intent(ToDoListActivity.this,TasksActivity.class);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/PMR/ToDoList/Controller/ToDoListAdapter.java
+++ app/src/main/java/PMR/ToDoList/Controller/ToDoListAdapter.java
@@ -3,6 +3,7 @@ package PMR.ToDoList.Controller;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,17 +17,29 @@ import PMR.ToDoList.R;
 public class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.ToDoListViewHolder> {
 
     private ArrayList<ToDoList> toDoLists;
+    private OnItemClickListener toDoListListener;
 
     public ToDoListAdapter(ArrayList<ToDoList> toDoLists){
 
         this.toDoLists=toDoLists;
     }
 
+    // INTERFACE ONITEMCLICKLISTENER POUR ECOUTER A L'INTERIEUR
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        toDoListListener=listener;
+    }
+
     @NonNull
     @Override
     public ToDoListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_todolist,parent,false);
-        ToDoListViewHolder tdlv=new ToDoListViewHolder(v);
+        ToDoListViewHolder tdlv=new ToDoListViewHolder(v, toDoListListener);
         return tdlv;
     }
 
@@ -41,13 +54,45 @@ public class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.ToDoLi
         return toDoLists.size();
     }
 
+
+
+    // CLASS TODOLISTVIEWHOLDER
+
     public static class ToDoListViewHolder extends RecyclerView.ViewHolder{
 
         public TextView nameToDoList;
+        public ImageView btnDeleteToDoList;
 
-        public ToDoListViewHolder(@NonNull View itemView) {
+        public ToDoListViewHolder(@NonNull View itemView, final OnItemClickListener toDoListListener) {
             super(itemView);
             nameToDoList = itemView.findViewById(R.id.textTodoList);
+            btnDeleteToDoList=itemView.findViewById(R.id.btnDeleteToDoList);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (toDoListListener !=null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            toDoListListener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            btnDeleteToDoList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (toDoListListener !=null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            toDoListListener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
+
         }
     }
 
