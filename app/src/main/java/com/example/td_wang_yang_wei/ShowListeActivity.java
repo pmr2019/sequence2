b03357b4fad12f72b75fe0d3f3bc5bc3089d58a0
SiyuanWANG0822 <sarah0822lille@gmail.com>
2019-05-30 18:09:52 +0200

    showlist and show(item)

---
+++ app/src/main/java/com/example/td_wang_yang_wei/ShowListeActivity.java
@@ -0,0 +1,15 @@
+package com.example.td_wang_yang_wei;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowListeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_liste);
+    }
+}
