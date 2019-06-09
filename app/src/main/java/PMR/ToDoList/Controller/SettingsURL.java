f96a34c8d0d289d1358bc117db6fad3132d81d59
Pierrick Gervasi <Pierrick Gervasi@gmail.com>
2019-06-09 12:30:43 +0200

    Vérification connexion internet + settingsURL

--- app/src/main/java/PMR/ToDoList/Controller/SettingsURL.java
+++ app/src/main/java/PMR/ToDoList/Controller/SettingsURL.java
@@ -1,6 +1,7 @@
 package PMR.ToDoList.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.EditText;
@@ -11,11 +12,15 @@ import PMR.ToDoList.R;
 public class SettingsURL extends AppCompatActivity {
 
     private EditText urlApi;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_url);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         urlApi=findViewById(R.id.urlApi);
 
